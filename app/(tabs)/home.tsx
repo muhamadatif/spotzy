f4ecfda4591d7317b50825c3462107a0a807faa1
Mohamed Atif <mohamedabotreka40@gmail.com>
2025-03-24 15:51:04 +0200

    using webhooks to authenticate and link clerk with convex database

--- app/(tabs)/home.tsx
+++ app/(tabs)/home.tsx
@@ -1,10 +1,17 @@
 import { Image, Pressable, Text, TouchableOpacity, View } from "react-native";
 import { styles } from "../../styles/auth.styles";
 import { Link } from "expo-router";
+import { useAuth } from "@clerk/clerk-expo";
 export default function Index() {
+  const { signOut } = useAuth();
   return (
     <View style={styles.container}>
       <Link href="/notifications">visit notifications screen</Link>
+      <TouchableOpacity style={styles.container} onPress={() => signOut()}>
+        <Text style={{ color: "white", padding: 4, backgroundColor: "red" }}>
+          Signout
+        </Text>
+      </TouchableOpacity>
     </View>
   );
 }
---
+++ convex/http.ts
@@ -0,0 +1,70 @@
+import { httpRouter } from "convex/server";
+import { httpAction } from "./_generated/server";
+import { Webhook } from "svix";
+import { api } from "./_generated/api";
+
+const http = httpRouter();
+
+http.route({
+  path: "/clerk-webhook",
+  method: "POST",
+  handler: httpAction(async (ctx, req) => {
+    const webhookSecret = process.env.CLERK_WEBHOOK_SECRET;
+    if (!webhookSecret) {
+      throw new Error("Clerk webhook secret not set");
+    }
+
+    // check headers
+    const svix_id = req.headers.get("svix-id");
+    const svix_signature = req.headers.get("svix-signature");
+    const svix_timestamp = req.headers.get("svix-timestamp");
+
+    if (!svix_id || !svix_signature || !svix_timestamp) {
+      return new Response("Error occured -- no svix headers", { status: 400 });
+    }
+
+    const payload = await req.json();
+    const body = JSON.stringify(payload);
+
+    const wh = new Webhook(webhookSecret);
+
+    let evt: any;
+    try {
+      evt = wh.verify(body, {
+        "svix-id": svix_id,
+        "svix-signature": svix_signature,
+        "svix-timestamp": svix_timestamp,
+      }) as any;
+    } catch (error) {
+      console.error("Error verifying webhook", error);
+      return new Response("Error occured", { status: 400 });
+    }
+
+    const eventType = evt.type;
+
+    if (eventType === "user.created") {
+      const { id, email_addresses, first_name, last_name, image_url } =
+        evt.data;
+
+      const email = email_addresses[0].email_address;
+      const name = `${first_name || ""} ${last_name || ""}`.trim();
+
+      try {
+        await ctx.runMutation(api.users.createUser, {
+          email,
+          fullname: name,
+          image: image_url,
+          clerkId: id,
+          username: email.split("@")[0],
+        });
+      } catch (error) {
+        console.log(error);
+        return new Response("Error creatin user", { status: 500 });
+      }
+    }
+
+    return new Response("OK", { status: 200 });
+  }),
+});
+
+export default http;
